92970a9dfb3668ba3f3ae7611919d89e29569bd8
adamraider <adamjraider@gmail.com>
2015-04-22 01:44:22 -0400

    Gulp update

--- gulpfile.js
+++ gulpfile.js
@@ -4,32 +4,43 @@ var jade = require('gulp-jade');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var coffee = require('gulp-coffee');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', ['sass', 'jade', 'watch']);
+gulp.task('default', ['serve']);
 
 gulp.task('sass', function() {
     gulp.src('./src/sass/**/*.sass')
 		.pipe(sass({indentedSyntax: true}))
-		.pipe(gulp.dest('./dist/css/'));
+		.pipe(gulp.dest('./dist/css/'))
+		.pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('jade', function() {
   	gulp.src('./src/jade/*.jade')
 		.pipe(jade({pretty: true}))
-		.pipe(gulp.dest('./dist/'));
+		.pipe(gulp.dest('./dist/'))
+		.pipe(browserSync.reload({stream:true}));
 });
-		  
-
-gulp.task('watch', function() {
-    
-});
-
-
 
 gulp.task('coffee', function() {
 	gulp.src('./src/coffee/*.coffee')
 		.pipe(coffee())
 		.pipe(concat('scripts.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('./dist/js/'));
+		.pipe(gulp.dest('./dist/js/'))
+		.pipe(browserSync.reload({stream:true}));
+});
+
+
+gulp.task('serve', function() {
+	browserSync.init({
+		server: {
+            baseDir: "./dist/"
+        }
+	});
+	
+	gulp.watch('./src/sass/**/*.sass', ['sass']);
+	gulp.watch('./src/jade/**/*.jade', ['jade']);
+	gulp.watch('./src/coffee/**/*.coffee', ['coffee']);
+	
 });
