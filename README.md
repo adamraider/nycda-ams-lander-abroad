76f8d3becc7431a5260734c9eb6041567c1e1bf1
adamraider <adamjraider@gmail.com>
2015-05-01 16:32:24 -0400

    remove node

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# starter-kit
+starter-kit
+==============
 An HTML5 project template utilizing Sass, Jade, and Gulp. As well as Bourbon and Neat for styles.
+
+## Instructions
+    git clone http://github.com/adamraider/starter-kit.git new-project
+    cd new-project
+    npm install
+    gulp
--- gulpfile.js
+++ gulpfile.js
@@ -4,40 +4,57 @@ var jade = require('gulp-jade');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var coffee = require('gulp-coffee');
+var imagemin = require('gulp-imagemin');
 var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
-gulp.task('default', ['jade', 'sass', 'coffee', 'serve']);
+gulp.task('default', ['jade', 'sass', 'image', 'js', 'serve']);
 
 gulp.task('sass', function() {
     gulp.src('./src/sass/**/*.sass')
 		.pipe(sass({indentedSyntax: true}))
-		.pipe(gulp.dest('./dist/css/'))
-		.pipe(browserSync.reload({stream: true}));
+		.pipe(gulp.dest('./build/css/'))
+		.pipe(reload({stream: true}));
 });
 
 gulp.task('jade', function() {
   	gulp.src('./src/jade/*.jade')
 		.pipe(jade({pretty: true}))
-		.pipe(gulp.dest('./dist/'))
-		.pipe(browserSync.reload({stream:true}));
+		.pipe(gulp.dest('./build/'))
+		.pipe(reload({stream: true}));
 });
+
+gulp.task('image', function () {
+   gulp.src('.src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/img/'))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('js', function () {
+   gulp.src('.src/js/*')
+        .pipe(uglify())
+        .pipe(gulp.dest('.build/js/'));
+});
+
 
 gulp.task('coffee', function() {
 	gulp.src('./src/coffee/*.coffee')
 		.pipe(coffee())
 		.pipe(concat('scripts.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('./dist/js/'))
-		.pipe(browserSync.reload({stream:true}));
+		.pipe(gulp.dest('./build/js/'))
+		.pipe(reload({stream: true}));
 });
 
 gulp.task('serve', function() {
     browserSync.init({
 		server: {
-            baseDir: "./dist/"
+            baseDir: "./build/"
         }
 	});
 	gulp.watch('./src/sass/**/*.sass', ['sass']);
 	gulp.watch('./src/jade/**/*.jade', ['jade']);
-	gulp.watch('./src/coffee/**/*.coffee', ['coffee']);
+    gulp.watch('./src/img/**/*', ['image']);
+    gulp.watch('./src/js/**/*.js', ['js']);
 });
