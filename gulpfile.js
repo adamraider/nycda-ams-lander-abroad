8062394d7a7c7004c11e64feea47a4f3e18fd206
Adam Raider <adamjraider@gmail.com>
2015-10-24 17:20:02 -0400

    starter-kit v2

--- gulpfile.js
+++ gulpfile.js
@@ -12,50 +12,50 @@ var reload = browserSync.reload;
 gulp.task('default', ['jade', 'sass', 'image', 'js', 'serve']);
 
 gulp.task('sass', function() {
-    gulp.src('./src/sass/main.sass')
-		.pipe(sass({indentedSyntax: true}).on('error', util.log))
-		.pipe(gulp.dest('./build/css/'))
-		.pipe(reload({stream: true}));
+  gulp.src('./src/sass/application.sass')
+    .pipe(sass({indentedSyntax: true}).on('error', util.log))
+    .pipe(gulp.dest('./build/css/'))
+    .pipe(reload({stream: true}));
 });
 
 gulp.task('jade', function() {
-  	gulp.src('./src/jade/*.jade')
-		.pipe(jade({pretty: true}).on('error', util.log))
-		.pipe(gulp.dest('./build/'))
-		.pipe(reload({stream: true}));
+	gulp.src('./src/jade/*.jade')
+  	.pipe(jade({pretty: true}).on('error', util.log))
+  	.pipe(gulp.dest('./build/'))
+  	.pipe(reload({stream: true}));
 });
 
 gulp.task('image', function () {
-   gulp.src('./src/img/*')
-        .pipe(imagemin().on('error', util.log))
-        .pipe(gulp.dest('./build/img/'))
-        .pipe(reload({stream: true}));
+ gulp.src('./src/img/*')
+  .pipe(imagemin().on('error', util.log))
+  .pipe(gulp.dest('./build/img/'))
+  .pipe(reload({stream: true}));
 });
 
 gulp.task('js', function () {
-   gulp.src('./src/js/**/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./build/js/'));
+  gulp.src('./src/js/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js/'));
 });
 
 gulp.task('coffee', function() {
-	gulp.src('./src/coffee/*.coffee')
-		.pipe(coffee().on('error', util.log))
-		.pipe(concat('scripts.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('./build/js/'))
-		.pipe(reload({stream: true}));
+  gulp.src('./src/coffee/*.coffee')
+    .pipe(coffee().on('error', util.log))
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js/'))
+    .pipe(reload({stream: true}));
 });
 
 gulp.task('serve', function() {
-    browserSync.init({
-		server: {
-            baseDir: "./build/"
-        }
-	});
-	gulp.watch('./src/sass/**/*.sass', ['sass']);
-    gulp.watch('./src/sass/**/*.scss', ['sass']);
-	gulp.watch('./src/jade/**/*.jade', ['jade']);
-    gulp.watch('./src/img/**/*', ['image']);
-    gulp.watch('./src/js/**/*.js', ['js']);
+  browserSync.init({
+    server: {
+      baseDir: "./build/"
+    }
+  });
+  gulp.watch('./src/sass/**/*.sass', ['sass']);
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+  gulp.watch('./src/jade/**/*.jade', ['jade']);
+  gulp.watch('./src/img/**/*', ['image']);
+  gulp.watch('./src/js/**/*.js', ['js']);
 });
