1d27d7a6e7cf748d8389a6ec1b87d773ee425b08
Adam Raider <adamjraider@gmail.com>
2015-11-23 10:57:07 -0500

    build

---
+++ js/script.js
@@ -0,0 +1,1 @@
+array=document.getElementsByClassName("strict-height");for(var i=0;i<array.length;i++)content=array[i],height=content.getElementsByClassName("text")[0].scrollHeight+140,content.style.height=height+"px";
