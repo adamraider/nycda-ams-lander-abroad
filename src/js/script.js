eeed1150db65d64de78dcba2706ac026ee51b70d
Adam Raider <adamjraider@gmail.com>
2015-11-23 10:54:42 -0500

    responsive fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 array = document.getElementsByClassName("strict-height")
 for(var i = 0; i < array.length; i++){
   content = array[i]
-  height = content.getElementsByClassName("text")[0].scrollHeight + 120
+  height = content.getElementsByClassName("text")[0].scrollHeight + 140
   content.style.height = height + "px"
 }
