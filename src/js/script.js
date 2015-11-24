155cc273fbf77a9c1b5e37498464f04a5a2705f7
Adam Raider <adamjraider@gmail.com>
2015-11-24 10:32:32 -0500

    height match on page resize

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,45 @@
-array = document.getElementsByClassName("strict-height")
-for(var i = 0; i < array.length; i++){
-  content = array[i]
-  height = content.getElementsByClassName("text")[0].scrollHeight + 140
-  content.style.height = height + "px"
+(function($,sr){
+
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+	  var timeout;
+
+	  return function debounced () {
+		  var obj = this, args = arguments;
+		  function delayed () {
+			  if (!execAsap)
+				  func.apply(obj, args);
+			  timeout = null;
+		  };
+
+		  if (timeout)
+			  clearTimeout(timeout);
+		  else if (execAsap)
+			  func.apply(obj, args);
+
+		  timeout = setTimeout(delayed, threshold || 100);
+	  };
+  }
+  // smartresize 
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
+
+
+// usage:
+$(window).smartresize(function(){
+	strictHeightMatch()
+});
+
+strictHeightMatch()
+
+function strictHeightMatch(){
+	// height matcher
+	array = document.getElementsByClassName("strict-height")
+	for(var i = 0; i < array.length; i++){
+		content = array[i]
+		height = content.getElementsByClassName("text")[0].scrollHeight + 120
+		content.style.height = height + "px"
+	}
 }
